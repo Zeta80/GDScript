e91faa52c4a512bac996f7056bd65c1cc86e87df
Zeta80 <axxel1700@gmail.com>
2025-02-03 03:24:19 +0100

    variables 1.0

--- main.gd
+++ main.gd
@@ -1,16 +1,17 @@
 extends Node
 
+var health = 100
 @onready var label: Label = $Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	label.text = "Hellooo spoilerr"
+	label.text = str(health)
 	label.modulate = Color.AQUAMARINE
 
 func _input(event):
 	if event.is_action_pressed("my_action"):
-		label.text = "fabietoooo"
-		label.modulate = Color.DARK_RED
+		health -= 10
+		label.modulate = Color.RED
+		label.text = str(health)
 	if event.is_action_released("my_action"):
-		label.text = "Hellooo spoilerr"
 		label.modulate = Color.AQUAMARINE
