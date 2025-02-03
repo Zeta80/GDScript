d18d160c07ed559081e6a857bac5a7e6e00104b1
Zeta80 <axxel1700@gmail.com>
2025-02-03 03:20:03 +0100

    input

--- main.gd
+++ main.gd
@@ -4,5 +4,13 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	label.text = "Hellooo spoilerr"s
+	label.text = "Hellooo spoilerr"
 	label.modulate = Color.AQUAMARINE
+
+func _input(event):
+	if event.is_action_pressed("my_action"):
+		label.text = "fabietoooo"
+		label.modulate = Color.DARK_RED
+	if event.is_action_released("my_action"):
+		label.text = "Hellooo spoilerr"
+		label.modulate = Color.AQUAMARINE
