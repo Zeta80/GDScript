d1459d2c8866549b976abd21f2dd273ab2308df2
Zeta80 <axxel1700@gmail.com>
2025-02-05 20:54:19 +0100

    restore array

--- main.gd
+++ main.gd
@@ -5,13 +5,13 @@ var health = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	label.text = str(health)
-	label.modulate = Color.AQUAMARINE
-
-func _input(event):
-	if event.is_action_pressed("my_action"):
-		health -= 10
-		label.modulate = Color.RED
-		label.text = str(health)
-	if event.is_action_released("my_action"):
-		label.modulate = Color.AQUAMARINE
+	var items: Array[String] = ["ciao", "culo", "chiappe"]
+	print(items)
+	
+	items[1] = "forfora"
+	print(items)
+	
+	items.remove_at(1)
+	print("post remove",items)
+	items.append("culatiello")
+	print(items)
