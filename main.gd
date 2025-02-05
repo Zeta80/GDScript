9b1f4e3eff89edb5f0cb13ee0d86daebdb9feb69
Zeta80 <axxel1700@gmail.com>
2025-02-05 20:58:10 +0100

    loop

--- main.gd
+++ main.gd
@@ -8,10 +8,8 @@ func _ready() -> void:
 	var items: Array[String] = ["ciao", "culo", "chiappe"]
 	print(items)
 	
-	items[1] = "forfora"
-	print(items)
-	
-	items.remove_at(1)
-	print("post remove",items)
-	items.append("culatiello")
-	print(items)
+	for item in items:
+		if item.length() < 6:
+			print(item)
+		else:
+			print(item + " troppo lungo")
