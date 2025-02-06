49c19965258eb44df173464dbd0f0825732e514c
Zeta80 <axxel1700@gmail.com>
2025-02-06 01:07:32 +0100

    classes

--- main.gd
+++ main.gd
@@ -1,17 +1,7 @@
 extends Node
 
-var health = 100
-@onready var label: Label = $Label
+@export var cahara_death: Character
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var pp = {}
-	var players = {
-		"carl" : {"level": 1, "vita": 140},
-		"jason" : {"level": 10, "vita": 240}, 
-		"pirton" : {"level": 21, "vita": 1450}, 
-		"cobain" : {"level": 61, "vita": 160}, 
-		}
-		
-	for username in players:
-		print(username + "; " + str(players[username]))
+	cahara_death.chara_die()
